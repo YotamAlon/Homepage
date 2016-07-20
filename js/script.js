15b113510194fa063c6b7968f3ddd1c15f86c2da
YotamAlon <yotamalon@gmail.com>
2016-07-20 18:11:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var showLoader = function () {
-    document.querySelector("#content").innerHTML = "<div class='text-center'><img src='images/ajax-loader.gif'></div>"
+    document.querySelector("#content").innerHTML = "<div class='text-center'><img src='images/ajax-loader.gif'></div>";
 }
 
 var insertHtml(html) {
-    document.querySelector("#content").innerHTML = html
+    document.querySelector("#content").innerHTML = html;
 }
 var loadHtml = function (name) {
     showLoader();
     $.get(name, insertHtml(data));
-    console.log($.get(name, insertHtml(data)))
+    console.log($.get(name, insertHtml(data)));
 }
