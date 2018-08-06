ccfb3b3c916a3c3fa2893960a6db8da9f901cf77
Yotam Alon <yotamalon@gmail.com>
2018-08-06 13:17:45 +0300

    Added basic models, view functions and templates

--- Homepage/models.py
+++ Homepage/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=256)
+    date = models.DateField()
+    description = models.TextField()
+
+
+class Resume(models.Model):
+    pdf_file = models.FileField(null=True, blank=True)
+    doc_file = models.FileField(null=True, blank=True)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=256)
+    link = models.CharField(max_length=512)
+    description = models.TextField()
+    status = models.CharField(max_length=64)
+
+
+class Resource(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    link = models.CharField(max_length=512)
+    package = models.FileField(null=True, blank=True)
+
+
+class Idea(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
--- Homepage/views.py
+++ Homepage/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
-# Create your views here.
+
+def index(request):
+    return render_to_response('homepage.html')
+
+
+def resume(request):
+    from Homepage.models import Resume
+    resume = Resume.objects.all().first()
+    return render_to_response('resume.html', {'resume': resume})
+
+
+def projects(request):
+    from Homepage.models import Project
+    projects = Project.object.all()
+    return render_to_response('projects.html', {'projects': projects})
+
+
